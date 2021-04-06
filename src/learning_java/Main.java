e62f927d065820283c618f981f4fd5b10b4a8367
user name <user email>
2021-04-06 20:49:26 +0530

    PArameters in JAva

--- src/learning_java/Main.java
+++ src/learning_java/Main.java
@@ -2,24 +2,19 @@ package learning_java;
 import java.util.Scanner;
 public class Main {
 	
-	public static void announceDeveloperTeaTime() {
-		System.out.println("Waiting for developer tea time...");
-		System.out.println("Type in a random word and press Enter to start developer tea time");
-		Scanner input = new Scanner(System.in);
-		input.next();
-		System.out.println("It's developer tea time!");
+	public static void calculateTotalMealPrice(double listedMealPrice,
+			                                   double tipRate,
+			                                   double taxRate) {
+		double tip = tipRate * listedMealPrice;
+		double tax = taxRate * listedMealPrice;
+		double result = listedMealPrice + tip + tax;
+		System.out.println("Your total meal price is " + result);
 	}
+	
+	
 	public static void main(String[] args) {
-		System.out.println("Welcome to your new job");
-		
-		announceDeveloperTeaTime();
-		
-		System.out.println("Write code");
-		System.out.println("Review code");
-		
-		announceDeveloperTeaTime();
-		
-		System.out.println("Get promoted!");
+		calculateTotalMealPrice(15,.2,.08);
+		calculateTotalMealPrice(25,.18,.08);
 	}
 }
  
