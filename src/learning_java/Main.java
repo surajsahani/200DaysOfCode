7ca351bda292b5e795a111bd051206e65542923b
user name <user email>
2021-04-06 22:20:26 +0530

    Constructors

--- src/learning_java/Main.java
+++ src/learning_java/Main.java
@@ -2,22 +2,25 @@ package learning_java;
 import java.util.Scanner;
 public class Main {
 	
-	public static double calculateTotalMealPrice(double listedMealPrice,
-			                                   double tipRate,
-			                                   double taxRate) {
-		double tip = tipRate * listedMealPrice;
-		double tax = taxRate * listedMealPrice;
-		double result = listedMealPrice + tip + tax;
-		return result;
+	public static double salaryCalculator(double hoursPerWeek, 
+			                              double amountPerHour,
+			                              int vacationDays) {
+		if (hoursPerWeek < 0) {
+			return -1;
+		}
+		
+		if (amountPerHour < 0) {
+			return -1;
+		}
+		
+		double weeklyPayCheck = hoursPerWeek * amountPerHour;
+		double unpaidTime = vacationDays * amountPerHour * 8;
+		return (weeklyPayCheck * 52) - unpaidTime;
 	}
 	
-	
 	public static void main(String[] args) {
-		double groupTotalMealPrice = calculateTotalMealPrice(100,.0,.08);
-		System.out.println(groupTotalMealPrice);
-		
-		double individualMealPrice = groupTotalMealPrice / 5;
-		System.out.println(individualMealPrice);
+		double salary = salaryCalculator(40,15,8);
+		System.out.println(salary);
 	}
 }
  
