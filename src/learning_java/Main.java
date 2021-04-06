e24ce545f31c99ae1199b0caaa028d9a0462bc7d
user name <user email>
2021-04-06 21:55:21 +0530

    Return types in java

--- src/learning_java/Main.java
+++ src/learning_java/Main.java
@@ -2,19 +2,22 @@ package learning_java;
 import java.util.Scanner;
 public class Main {
 	
-	public static void calculateTotalMealPrice(double listedMealPrice,
+	public static double calculateTotalMealPrice(double listedMealPrice,
 			                                   double tipRate,
 			                                   double taxRate) {
 		double tip = tipRate * listedMealPrice;
 		double tax = taxRate * listedMealPrice;
 		double result = listedMealPrice + tip + tax;
-		System.out.println("Your total meal price is " + result);
+		return result;
 	}
 	
 	
 	public static void main(String[] args) {
-		calculateTotalMealPrice(15,.2,.08);
-		calculateTotalMealPrice(25,.18,.08);
+		double groupTotalMealPrice = calculateTotalMealPrice(100,.0,.08);
+		System.out.println(groupTotalMealPrice);
+		
+		double individualMealPrice = groupTotalMealPrice / 5;
+		System.out.println(individualMealPrice);
 	}
 }
  
