9a65fb2c10ff80d0bf4f9ccb486663bdd012c25d
suraj <surajkumarsahani1997@gmail.com>
2021-04-14 14:24:00 +0530

    primitive values

---
+++ src/JavaEssentialTraining/PrimitiveValues.java
@@ -0,0 +1,21 @@
+package JavaEssentialTraining;
+
+import java.text.NumberFormat;
+
+public class PrimitiveValues {
+	public static void main(String[] args) {
+		int intValue = 42;
+		String fromInt = Integer.toString(intValue);
+		System.out.println(fromInt);
+		
+		boolean boolValue = true;
+		String fromBool = Boolean.toString(boolValue);
+		System.out.println(fromBool);
+		
+		long longValue = 10_000_000;
+		NumberFormat formatter = NumberFormat.getNumberInstance();
+		String formatted = formatter.format(longValue);
+		System.out.println(formatted);
+		
+	}
+}
--- src/JavaEssentialTraining/StringClass.java
+++ src/JavaEssentialTraining/StringClass.java
@@ -5,6 +5,9 @@ public class StringClass {
 		String s1 = "This is s String!";
 		System.out.println(s1);
 		
+		String s2 = new String("this is also a String!");
+		System.out.println(s2);
+		
 		String s3 = "Shirt size:";
 		String s4 = "M";
 		String s5 = s3 + s4 + ", Qty: " + 4;
