ccd45d0f7229247c8e84d3fb5150bbb3bf030148
suraj <surajkumarsahani1997@gmail.com>
2021-04-15 17:56:18 +0530

    methods

---
+++ src/JavaEssentialTraining/Methods.java
@@ -0,0 +1,29 @@
+package JavaEssentialTraining;
+
+public class Methods {
+
+	static String[] months = { "January", "February", "March", 
+			"April", "May", "June", 
+			"July", "August", "September",
+			"October", "November", "December" };
+
+	public static void main(String[] args) {
+		loopIt("Months of the year");
+		loopIt("Second run");
+		loopIt("Third run");
+	}
+
+
+	static void loopIt(String label) {
+		
+		System.out.println(label);
+		for (int i = 0; i < label.length(); i++) {
+			System.out.print("*");
+		}
+		System.out.println("");
+
+		for (int i = 0; i < months.length; i++) {
+			System.out.println(months[i]);
+		}
+	}
+}
