f7d46c4a9bf4e58b7d8dc4509e82e59bdce4c521
suraj <surajkumarsahani1997@gmail.com>
2021-04-15 11:46:43 +0530

    eclipse debugging

---
+++ src/JavaEssentialTraining/debugging.java
@@ -0,0 +1,14 @@
+package JavaEssentialTraining;
+
+public class debugging {
+	
+	public static void main(String[] args) {
+		
+		String welcome = "Welcome!";
+		char[] chars = welcome.toCharArray();
+		
+		for(int i =0; i <chars.length; i++) {
+			System.out.println(chars[i]);
+		}
+	}
+}
