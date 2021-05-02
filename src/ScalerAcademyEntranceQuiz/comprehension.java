b4081434eb639ff7764c478af3cdcc6dd86e6920
suraj <surajkumarsahani1997@gmail.com>
2021-05-02 12:00:21 +0530

    IBM coding question

--- src/ScalerAcademyEntranceQuiz/comprehension.java
+++ src/ScalerAcademyEntranceQuiz/comprehension.java
@@ -2,4 +2,20 @@ package ScalerAcademyEntranceQuiz;
 
 public class comprehension {
 
+	static int printlTribeRec(int n) {
+		if (n ==0 || n ==1 || n==2)
+			return 0;
+		
+		if (n == 3)
+			return 1
+			else 
+				return printTribeRE(n - 1) +
+						printTribeRec(n -2) +
+						printTribeRec(n - 3)''
+	}
+	
+	static void printTrib(int n) {
+		for (int i =1; i<n; i++)
+			System.out.println(printTribeRec(i)+"");
+	}
 }
--- src/ScalerAcademyEntranceQuiz/function.java
+++ src/ScalerAcademyEntranceQuiz/function.java
@@ -2,4 +2,16 @@ package ScalerAcademyEntranceQuiz;
 
 public class function {
 
+
+		void function(){
+		     int i = 0;
+		     int sum = 0;
+		     while(i < 100) {
+		         sum = sum + i;
+		         sum = i + sum;
+		         i +=1;
+		     }
+		     System.out.println(sum);
+		
+		}
 }
