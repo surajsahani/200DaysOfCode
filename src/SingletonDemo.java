639258fbac3dcca07b5d218665e3b51b7501ada2
suraj <surajkumarsahani1997@gmail.com>
2021-06-03 12:37:29 +0530

    singleton method

---
+++ src/SingletonDemo.java
@@ -0,0 +1,8 @@
+// File Name: SingletonDemo.java
+public class SingletonDemo {
+
+   public static void main(String[] args) {
+      Singleton tmp = Singleton.getInstance( );
+      tmp.demoMethod( );
+   }
+}
